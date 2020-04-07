67ad02023a10b39933f8562b462759039845c3b5
Tanner Townsend <rsmcwow@gmail.com>
2020-04-07 02:16:48 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,13 +15,13 @@ class Transfer
   end
 
   def execute_transaction
-    if not self.valid? && self.status == "pending"
-      self.status = "rejected"
-      "Transaction rejected. Please check your account balance."
-    else
+    if self.valid? && self.status == "pending"
       self.sender.balance -= self.amount
       self.receiver.balance += self.amount
       self.status = "complete"
+    else
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."
     end
   end
 
