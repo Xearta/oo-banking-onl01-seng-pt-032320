e41d4ca28b7df52ca9527e6b078b8b91ced979d9
Tanner Townsend <rsmcwow@gmail.com>
2020-04-07 02:10:59 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,9 +20,8 @@ class Transfer
       self.receiver.balance += self.amount
       self.status = "complete"
     else
-
       self.status = "rejected"
-      return "Transaction rejected. Please check your account balance."
+      "Transaction rejected. Please check your account balance."
     end
   end
 
