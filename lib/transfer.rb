866c529664d9807e20339e69f5495460a07de7ca
Tanner Townsend <rsmcwow@gmail.com>
2020-04-07 02:13:56 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,7 +15,7 @@ class Transfer
   end
 
   def execute_transaction
-    if self.sender.balance > self.amount && self.status == "pending"
+    if self.valid? && self.status == "pending"
       self.sender.balance -= self.amount
       self.receiver.balance += self.amount
       self.status = "complete"
